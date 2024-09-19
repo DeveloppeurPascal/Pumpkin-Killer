d05dbde4ab99fcbc65589355a90919e44386a12a
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-09-19 19:10:20 +0200

    added header copyright comments (#55)

--- src/fMain.pas
+++ src/fMain.pas
@@ -1,3 +1,35 @@
+/// <summary>
+/// ***************************************************************************
+///
+/// Pumpkin Killer
+///
+/// Copyright 2018-2024 Patrick Prémartin under AGPL 3.0 license.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+/// DEALINGS IN THE SOFTWARE.
+///
+/// ***************************************************************************
+///
+/// Author(s) :
+/// Patrick PREMARTIN
+///
+/// Site :
+/// https://pumpkinkiller.gamolf.fr/
+///
+/// Project site :
+/// https://github.com/DeveloppeurPascal/Pumpkin-Killer
+///
+/// ***************************************************************************
+/// File last update : 2024-09-19T19:09:44.225+02:00
+/// Signature : 0cb5120e751ba251e4415ab7e20ca5530e0148dd
+/// ***************************************************************************
+/// </summary>
+
 unit fMain;
 
 interface
